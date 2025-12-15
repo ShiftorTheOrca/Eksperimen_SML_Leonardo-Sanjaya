b8b2bf605922b141d92e02c540a1e13152f08208
Leonardo Sanjaya <leonardosanjaya23@gmail.com>
2025-12-15 14:33:46 +0700

    Memperbaiki kesalahan code load & penyimpanan file

--- preprocessing/automate_Leonardo-Sanjaya.py
+++ preprocessing/automate_Leonardo-Sanjaya.py
@@ -3,9 +3,11 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 # Load Dataset
-df = pd.read_csv('../used_car_price_dataset_extended_raw.csv')
+df = pd.read_csv('used_car_price_dataset_extended_raw.csv')
 
 # Data Preprocessing
+df = df.drop_duplicates()
+
 # Ganti dari "NaN" jadi "No History" (field asli berisikan "None")
 df['service_history'] = df['service_history'].fillna('No History')
 
@@ -15,4 +17,4 @@ df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
 
 # Pilih fitur yang berkorelasi tinggi dengan target
 df_selected = df[['make_year', 'mileage_kmpl', 'engine_cc', 'owner_count', 'price_usd']]
-df_selected.to_csv("used_car_price_dataset_extended_preprocessing.csv", index=False)
+df_selected.to_csv('used_car_price_dataset_extended_preprocessing.csv', index=False)
